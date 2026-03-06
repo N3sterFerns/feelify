48aa666af9f2bd6becbbb15b73f6b3f82e53bd25
Nester <nesternash2004@gmail.com>
2026-03-06 11:16:07 +0530

    fixed register

--- FrontEnd/src/app.routes.jsx
+++ FrontEnd/src/app.routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
-import Register from "./features/auth/pages/register";
+import Register from "./features/auth/pages/Register";
 import Protected from "./features/auth/components/Protected";
 import Home from "./features/home/pages/Home";
 
